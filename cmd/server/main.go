b5b1a5dbc845069ccb6f203c39b3daece2fd45c8
Shane Gleeson <sgleeson@Shanes-iMac.local>
2021-02-13 15:29:14 -0800

    added health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/catpuking/go-rest-api-course/internal/transport/http"
 )
 
 //App - the struct which contains things like
@@ -12,6 +15,15 @@ type App struct{}
 // Run - Sets up our application
 func  (app *App) Run() error {
 	fmt.Println("Setting up our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
 	return nil
 }
 
